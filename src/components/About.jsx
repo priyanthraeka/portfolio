39d1d99e9e92df6b63ffe487fd66e1fc85acf4e0
Eka Priyanthara <priyanthraeka@pop-os.localdomain>
2023-06-17 20:21:52 +0800

    second commit

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,6 +1,8 @@
 import Card from "./Card";
 import profileImg from "../assets/0-removebg-preview.png";
 import Tilt from "react-parallax-tilt";
+import { BsEnvelope } from "react-icons/bs";
+
 const About = () => {
   return (
     <Card id="about">
@@ -21,24 +23,15 @@ const About = () => {
             <p className="font-bold text-lg text-gray-700">
               Frontend Developer
             </p>
-            <p className="text-sm text-gray-600">
-              I am a Full-Stack developer based in Pune, India. I am an
-              Information Technology undergraduate from SPPU. I am very
-              passionate about improving my coding skills & developing
-              applications & websites. I build WebApps and Websites using MERN
-              Stack. Working for myself to improve my skills. Love to build
-              Full-Stack clones.
+            <p className="text-base text-gray-600">
+              Saya adalah lulusan dari Institut Teknologi dan Bisnis STIKOM Bali
+              dengan jurusan Sistem Komputer. Saya memiliki minat bekerja
+              sebagai Frontend Developer.
             </p>
-            <p className="text-sm flex">
-              <span className="font-bold text-blue-500 w-[55px]">Surel</span>
-              <span className="font-bold text-blue-500 me-2">:</span>
-              <span className="text-gray-600">priyantharaeka067@gmail.com</span>
-            </p>
-            <p className="text-sm flex">
-              <span className="font-bold text-blue-500 w-[55px]">Tempat</span>
-              <span className="font-bold text-blue-500 me-2">:</span>
-              <span className="text-gray-600">Denpasar, Indonesia</span>
-            </p>
+            <div className="flex justify-center items-center gap-2 text-base text-gray-600">
+              <BsEnvelope size={18} />
+              <span>priyantharaeka067@gmail.com</span>
+            </div>
             <button className="font-bold text-sm border border-blue-500 rounded-3xl px-4 py-2 mt-5 bg-blue-500 text-white">
               <a
                 href="https://drive.google.com/file/d/1d9yMP0OWviz3B_SZr4f8FVDxIrX9noFQ/view?usp=drive_link"
